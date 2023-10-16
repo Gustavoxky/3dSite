169bffc9f95aa5b0f7d0b9f9755783f89d561bb1
Gustavoxky <gustavoxky@gmail.com>
2023-10-16 00:54:45 -0300

    update

---
+++ src/components/Boat.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import { useGLTF } from "@react-three/drei";
+
+
+export const Boat = ({ isMobile }) => {
+  const computer = useGLTF("./models/boat/model.gltf");
+
+  return (
+    <group>
+      <hemisphereLight intensity={1} />
+      <spotLight
+        position={[-20, 50, 10]}
+        angle={0.12}
+        penumbra={1}
+        intensity={1}
+        castShadow
+        shadow-mapSize={1024}
+      />
+      <pointLight intensity={1} />
+      <primitive
+        object={computer.scene}
+        scale={0.0015}
+        position={[0, -1, 0]}
+        rotation={[0, Math.PI / -1, 0]}
+      />
+    </group>
+  );
+};
+
+
---
+++ src/components/Ocean.jsx
@@ -0,0 +1,36 @@
+import * as THREE from 'three'
+import React, { useRef, useMemo } from 'react'
+import { extend, useThree, useLoader, useFrame } from '@react-three/fiber'
+import { OrbitControls } from '@react-three/drei'
+import { Water } from 'three-stdlib'
+
+extend({ Water })
+
+export const Ocean = () => {
+    const ref = useRef()
+    const gl = useThree((state) => state.gl)
+    const waterNormals = useLoader(THREE.TextureLoader, './images/waternormals.jpeg')
+    waterNormals.wrapS = waterNormals.wrapT = THREE.RepeatWrapping
+    const geom = useMemo(() => new THREE.PlaneGeometry(10000, 10000), [])
+    const config = useMemo(
+      () => ({
+        textureWidth: 1512,
+        textureHeight: 1512,
+        waterNormals,
+        sunDirection: new THREE.Vector3(),
+        sunColor: 0xffffff,
+        waterColor: 0x8ffff8,
+        distortionScale: 2,
+        fog: false,
+        format: gl.encoding
+      }),
+      [waterNormals]
+    )
+    useFrame((state, delta) => (ref.current.material.uniforms.time.value += delta))
+    
+    return (
+      <>
+        <water ref={ref} args={[geom, config]} rotation-x={-Math.PI / 2} position={[0, -1, 0]} />
+      </>
+    ) 
+  }
